095c384c4e9af3f719c1164f0a76ff1d87af690b
teddythetwig <teddythetwig@gmail.com>
2014-05-21 15:37:11 -0700

    added docker file

---
+++ railsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = '3e0cb1a5f9bfe1f1c0e8b5dd0e3bfa6c022b99a4d146cfab6077cf78580afe20e21f8e78a1f4286ff5db9329cc7d27616c91011360e2f801a8bef328b74d9146'
